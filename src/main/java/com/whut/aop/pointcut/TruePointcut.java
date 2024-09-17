6687de2dcd3c890eebe5361c5afedb9e7d9360f5
Whut2024 <349953@whut.edu.cn>
2024-09-17 18:13:19 +0800

    we finish a default true-pointcut by singleton pattern

---
+++ src/main/java/com/whut/aop/pointcut/TruePointcut.java
@@ -0,0 +1,32 @@
+package com.whut.aop.pointcut;
+
+import java.lang.reflect.Method;
+
+/**
+ * always matching
+ * @author whut2024
+ * @since 2024-09-17
+ */
+public class TruePointcut implements Pointcut {
+    @Override
+    public boolean matches(Method method, Class<?> targetClass) {
+        return true;
+    }
+
+    private TruePointcut(){}
+
+
+    private volatile static Pointcut pointcut;
+
+
+    public static Pointcut getInstance() {
+        if (pointcut == null) {
+            synchronized (TruePointcut.class) {
+                if (pointcut == null)
+                    pointcut = new TruePointcut();
+            }
+        }
+
+        return pointcut;
+    }
+}
